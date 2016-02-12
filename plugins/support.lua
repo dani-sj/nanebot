b0f913f239a5a1a11814b9ba110b5370da3ba7c7
abasshacker <Abbasserver00@gmail.com>
2016-02-12 16:49:38 +0330

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید                join 107513518!"
+  return "دستور زیر را به پیوی ربات ارسال کنید تا به گروه ساپورت وصل شوید                join! 107103055761
   end
 return {
   description = "shows persian support", 
