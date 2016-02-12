56f80d0c29202f3a7d2f9105ae9bcee3b1497c11
dani-sj <aliabuotorabi@gmail.com>
2016-02-13 02:48:34 +0330

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+639080540666", "extreme", "", ok_cb, false)
+send_contact(get_receiver(msg), "+989154123827", "evilbot", "", ok_cb, false)
 end
 
 return {
